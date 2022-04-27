483058697d3ec1bae9ea0d5edbe83a04be9a5606
kozlovpv1988@gmail.com <pavel.kozlov@playrix.com>
2022-04-27 14:01:53 +0500

    cast 02

--- test/code/cast/invokeMethod_cast.cpp
+++ test/code/cast/invokeMethod_cast.cpp
@@ -1,21 +1,24 @@
 #include "cast_class01.h"
 #include <functional>
+#include <iostream>
 
 namespace castNS
 {
 	void Example01();
+	void Example02();
 }
 
 void ExecuteCastTestCode()
 {
 	castNS::Example01();
+	castNS::Example02();
 }
 
 namespace castNS
 {
 	void Example01()
 	{
-		cCast0* cast0 = new cCast0_1();
+		cCast0* cast0 = new cCast0();
 
 		//dynamic_cast не компилиться если в классах нет виртуальных функций
 		//
@@ -37,13 +40,17 @@ namespace castNS
 		//	printf("dynamic_cast cCast0_2");
 		//}
 
-		if (static_cast<cCast0_1*>(cast0))
+		if (cCast0_1* cast01 = static_cast<cCast0_1*>(cast0))
 		{
+			//это по факту cCast0 поэтому будет мусор
+			std::cout << cast01->a1 << std::endl;
 			printf("static_cast cCast0_1\r\n");
 		}
 
-		if (static_cast<cCast0_2*>(cast0))
+		if (cCast0_2* cast02 = static_cast<cCast0_2*>(cast0))
 		{
+			//это по факту cCast0 поэтому будет мусор
+			std::cout << cast02->a2 << std::endl;
 			printf("static_cast cCast1_2\r\n");
 		}
 
@@ -53,6 +60,7 @@ namespace castNS
 			printf("dynamic_cast cCast1_1\r\n");
 		}
 
+		//каст не пройдет, не выведется
 		if (dynamic_cast<cCast1_2*>(cast1))
 		{
 			printf("dynamic_cast cCast1_2\r\n");
@@ -67,5 +75,17 @@ namespace castNS
 		{
 			printf("static_cast cCast1_2\r\n");
 		}
+	}
+
+	//void TestCast(cCast1_0& cast)
+
+	void Example02()
+	{
+		cCast1_1 cast1_1;
+		cast1_1.a1 = 1231231;
+		cCast1& cast1 = cast1_1;
+		cCast1_1 cast2 = dynamic_cast<cCast1_1&>(cast1_1);
+		cast1_1.a1 = 10;
+		cCast1_2 cast2_ = dynamic_cast<cCast1_2&>(cast1);
 	}
 }
