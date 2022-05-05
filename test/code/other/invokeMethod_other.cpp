b4f14b666dcf0b1e83a3026b09a5ea4b62ec6ae0
kozlovpv1988@gmail.com <pavel.kozlov@playrix.com>
2022-05-05 10:47:51 +0500

    other

--- test/code/other/invokeMethod_other.cpp
+++ test/code/other/invokeMethod_other.cpp
@@ -19,10 +19,29 @@ void ExecuteOtherTestCode()
 
 namespace otherNS
 {
+	void test(int i)
+	{
+		//тест чтобы посмотреть расстояние между переменными
+		//на дебаге по 32 байта между началами символов
+		//на релизе по 4 байта
+		int a = 10;
+		int b = 20;
+		int c = INT_MAX;
+		int d = 40;
+		std::cout << &a << ":" << &b << ":" << &c << ":" << &d << std::endl;
+		i++;
+		if (i < 20)
+			test(i + 1);
+		else
+		{
+			int aa = 30;
+			std::cout << &aa << std::endl;
+		}
+	}
+
 	void Example01()
 	{
-		
-
+		test(0);
 	}
 
 
