5841b81fe80551aff5adad0d5121c233b491f99c
kozlovpv1988@gmail.com <pavel.kozlov@playrix.com>
2022-05-09 13:09:34 +0500

    utils add

---
+++ test/utils/memoryWriter.h
@@ -0,0 +1,28 @@
+﻿//◦
+#pragma once
+#include "processInfo.h"
+#include <iostream>
+#include <string>
+
+namespace utils
+{
+    class MemoryWriter
+    {
+        long m_CurrentSize;
+    public:
+        MemoryWriter()
+        {
+            start();
+        }
+        void WriteDeltaMemory(std::string text = "")
+        {
+            long tempSize = utils::GetCurrentSize();
+            std::cout << tempSize - m_CurrentSize << " : "<< text << std::endl;
+            m_CurrentSize = tempSize;
+        }
+        void start()
+        {
+            m_CurrentSize = utils::GetCurrentSize();
+        }
+    };
+}
---
+++ test/utils/processInfo.h
@@ -0,0 +1,58 @@
+﻿//◦
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+#include "psapi.h"
+
+namespace utils
+{
+    void PrintMemoryInfo(DWORD processID)
+    {
+        HANDLE hProcess;
+        PROCESS_MEMORY_COUNTERS pmc;
+
+        // Print the process identifier.
+
+        printf("\nProcess ID: %u\n", processID);
+
+        // Print information about the memory usage of the process.
+
+        hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
+            PROCESS_VM_READ,
+            FALSE, processID);
+
+        if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc)))
+        {
+            printf("\tPageFaultCount: 0x%08X\n", pmc.PageFaultCount);
+            printf("\tPeakWorkingSetSize: 0x%08X\n",
+                pmc.PeakWorkingSetSize);
+            printf("\tWorkingSetSize: 0x%08X\n", pmc.WorkingSetSize);
+            printf("\tQuotaPeakPagedPoolUsage: 0x%08X\n",
+                pmc.QuotaPeakPagedPoolUsage);
+            printf("\tQuotaPagedPoolUsage: 0x%08X\n",
+                pmc.QuotaPagedPoolUsage);
+            printf("\tQuotaPeakNonPagedPoolUsage: 0x%08X\n",
+                pmc.QuotaPeakNonPagedPoolUsage);
+            printf("\tQuotaNonPagedPoolUsage: 0x%08X\n",
+                pmc.QuotaNonPagedPoolUsage);
+            printf("\tPagefileUsage: 0x%08X\n", pmc.PagefileUsage);
+            printf("\tPeakPagefileUsage: 0x%08X\n",
+                pmc.PeakPagefileUsage);
+        }
+
+        CloseHandle(hProcess);
+    }
+
+    int GetCurrentSize()
+    {
+        HANDLE hProcess;
+        PROCESS_MEMORY_COUNTERS pmc;
+        DWORD currentPid = GetCurrentProcessId();
+        hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
+            PROCESS_VM_READ,
+            FALSE, currentPid);
+        GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc));
+        CloseHandle(hProcess);
+        return pmc.WorkingSetSize;
+    }
+}
